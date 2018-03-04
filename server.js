40388fe899e12503e3e1bba097924e474aa3a831
oryam <16582185+oryam@users.noreply.github.com>
2018-03-04 19:52:42 +0100

    [server,dist] add static resources mapping for dist folder

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+
+const app = express();
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/hello', function (req, res) {
+  res.send('Hello World!');
+});
 
 app.listen(port, ip, 511, function () {
-  console.log(`Example app listening on port ${ip}:${port}!`)
-})
+  console.log(`Example app listening on port ${ip}:${port}!`);
+});
